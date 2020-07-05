9f5343959010f4f0eca2e3aadedc11c262eeba3e
paulajbastos <paulajbastos@paulajbastoss-MacBook-Air-2.local>
2020-07-05 12:16:44 -0300

    configurando Prettier com Eslint e alterando settings do vscode pro Prettier funcionar corretamente

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ type Props = {
   title: string
 }
 
-export default function Home({title = 'PAULA'} : Props) {
+export default function Home({ title = 'PAULA' }: Props) {
   return (
     <div className="container">
       <Head>
@@ -13,7 +13,7 @@ export default function Home({title = 'PAULA'} : Props) {
       </Head>
 
       <main>
-        <h1 className="title">{ title }</h1>
+        <h1 className="title">{title}</h1>
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
